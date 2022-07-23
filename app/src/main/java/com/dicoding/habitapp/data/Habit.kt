392c4a663e1f3b72d847fe17dfe91faa0c304438
alfan17 <alfan.indra17@gmail.com>
2022-07-23 10:41:04 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/habitapp/data/Habit.kt
@@ -0,0 +1,20 @@
+package com.dicoding.habitapp.data
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import org.jetbrains.annotations.NotNull
+
+//TODO 1 : Define a local database table using the schema in app/schema/habits.json
+@Parcelize
+@Entity(tableName = "habits")
+data class Habit(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val minutesFocus: Long,
+    val startTime: String,
+    val priorityLevel: String
+): Parcelable
